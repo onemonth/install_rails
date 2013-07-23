6d654c27dc170b0605ca9029320115f5fd7d20cb
Mattan Griffel <mattangriffel@gmail.com>
2013-07-23 00:51:56 -0700

    Remove active_record

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
