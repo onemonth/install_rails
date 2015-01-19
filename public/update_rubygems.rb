c82f87ff16deb0b7f08e96edd1a477fbad1e7110
Chris Oliver <excid3@gmail.com>
2015-01-19 14:59:50 -0600

    Add rubygems updates to public folder

---
+++ public/update_rubygems.rb
@@ -0,0 +1,60 @@
+require 'open-uri'
+
+class UpgradeRubygems
+  UPGRADES = {
+    "1.8" => "https://github.com/rubygems/rubygems/releases/download/v1.8.30/rubygems-update-1.8.30.gem",
+    "2.0" => "https://github.com/rubygems/rubygems/releases/download/v2.0.15/rubygems-update-2.0.15.gem",
+    "2.2" => "https://github.com/rubygems/rubygems/releases/download/v2.2.3/rubygems-update-2.2.3.gem"
+  }
+
+  def call
+    puts "You're up to date" and return if !upgrade_available?
+
+    puts "You currently have #{rubygems_version}"
+    download
+    install
+    update
+    puts "You now have #{rubygems_version}"
+  end
+
+  private
+
+    def download
+      puts "Downloading #{upgrade_url}..."
+
+      open(filename, "wb") do |file|
+        open(upgrade_url) do |uri|
+          file.write(uri.read)
+        end
+      end
+    end
+
+    def install
+      puts "Installing #{filename}..."
+      `gem install --local #{filename}`
+    end
+
+    def update
+      puts "Updating rubygems..."
+      `update_rubygems --no-ri --no-rdoc`
+    end
+
+    def rubygems_version
+      `gem --version`
+    end
+
+    def upgrade_available?
+      !!upgrade_url
+    end
+
+    def upgrade_url
+      @upgrade_file ||= UPGRADES[rubygems_version[0..2]]
+    end
+
+    def filename
+      @filename ||= upgrade_url.split("/").last
+    end
+end
+
+upgrade = UpgradeRubygems.new
+upgrade.call
